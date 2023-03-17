f8e498ef25134bb009b2efcef38c7ab804f3791f
Ronny Andersson <112690450+Problem111@users.noreply.github.com>
2023-03-17 09:54:42 +0100

    Add files via upload

---
+++ Javascript/ch21/java.js
@@ -0,0 +1,27 @@
+var friends = ["Lisa", "Jeff Bezos", "Elon Musk"];
+/*alert( friends[0] );
+alert(friends);*/
+console.log(friends)
+console.log(friends[2])
+
+var name = "Ronny"
+
+if (name == "Jennifer") {
+
+    console.log("Thats my name too!")
+}
+else {
+    console.log("Hi, " + name)
+}
+
+var myVariable = 2
+
+if(myVariable < 5) {
+    console.log("Upper")
+}
+if(myVariable > 5) {
+    console.log("Down")
+}
+
+for( var i = 0; i < 5; i++ ) {
+    console.log(i); }
